cedf4ace7c9bd458b564214289a865d067460c66
Lias <lias.israelsson@edu.lycksele.se>
2025-01-10 13:43:31 +0100

    first commit

---
+++ SkrivaTillFil/Program.cs
@@ -0,0 +1,7 @@
+﻿FileStream fs = new FileStream("text.txt", FileMode.Open, FileAccess.Read); 
+
+StreamReader sr = new StreamReader(fs);
+
+string sträng = sr.ReadToEnd();
+sr.Close();
+Console.WriteLine(sträng);
